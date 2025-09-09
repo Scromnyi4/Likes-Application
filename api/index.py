4740b748b2521335c7af269efd58b84ff7e45664
Scromnyi4 <burulsherova949@gmail.com>
2025-09-09 21:52:20 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -178,7 +178,7 @@ def handle_requests():
     key = request.args.get("key", "")
 
     # Ключ текшерүү
-    if key != "ScromnyiDev":
+    if key != "Scromnyi0606":
         return jsonify({"error": "Unauthorized. Invalid key."}), 403
 
     if not uid or not region:
